3b3c5a2bc233ca778653d0461479478a8b078551
Eduardo Campos <eduardocunha11@gmail.com>
2023-06-17 14:57:24 -0300

    problema ao conectar no bd

--- database/models/index.js
+++ database/models/index.js
@@ -16,14 +16,11 @@ const config = configs[env];
  */
 let sequelize;
 
-/*if (config.use_env_variable)
+if (config.use_env_variable)
 	sequelize = new Sequelize(process.env[config.use_env_variable], config);
 else
-	sequelize = new Sequelize(config.database, config.username, config.password, config);*/
+	sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-// sequelize = new Sequelize(config.database, config.username, config.password, config);
-
-sequelize = new Sequelize(config.database, config.username, config.password, config);
  
 const db = {
 	sequelize,
